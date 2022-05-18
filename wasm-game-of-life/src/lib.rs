8a36626767fe6f71cc88a6898a6966a64986023c
Jia Chao <jiachao2130@126.com>
2022-05-18 16:52:52 +0800

    wasm-game-of-life: can works, open http://localhost:8080

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
+use std::fmt;
 use wasm_bindgen::prelude::*;
+
+extern crate js_sys;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -16,4 +19,129 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, wasm-game-of-life!");
+}
+
+/// 生命游戏：
+/// 由一个二维网格所表示的无限宇宙，每个网格表示一个生命，生命的状态遵循下面四个规则：
+/// 1. 任何四周邻居存活数少于两个的存活网格将死亡
+/// 2. 任何四周邻居存活数为两个或三个的存活网格将在下一代继续存活
+/// 3. 任何四周邻居存活数多于三个的存活网格将死亡
+/// 4. 任何已经死亡的网格，如果周围邻居存活数为三个，将在下一代复活
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+
+/// 比如，一个三行三列的 Universe, 
+/// [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
+/// |  row0  |  row1  |  row2  |
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        // 随机生成 Cell 状态
+        let cells = (0..width * height)
+            .map(|_| {
+                if js_sys::Math::random() < 0.5 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_column in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_column == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_column = (column + delta_column) % self.width;
+                let index = self.get_index(neighbor_row, neighbor_column);
+                count += self.cells[index] as u8;
+            }
+        }
+
+        count
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let index = self.get_index(row, col);
+                let cell = self.cells[index];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match(cell, live_neighbors) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise,
+                };
+
+                next[index] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
 }
