d856913955753bbd69e0e029119148f648943778
Jia Chao <jiachao2130@126.com>
2022-05-18 17:49:48 +0800

    wasm-game-of-life: split impl for Universe, expose nesscessary APIs

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -86,28 +86,6 @@ impl Universe {
         self.to_string()
     }
 
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
-    }
-
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_column in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_column == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_column = (column + delta_column) % self.width;
-                let index = self.get_index(neighbor_row, neighbor_column);
-                count += self.cells[index] as u8;
-            }
-        }
-
-        count
-    }
-
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
         for row in 0..self.height {
@@ -129,6 +107,30 @@ impl Universe {
         }
 
         self.cells = next;
+    }
+}
+
+impl Universe {
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_column in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_column == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_column = (column + delta_column) % self.width;
+                let index = self.get_index(neighbor_row, neighbor_column);
+                count += self.cells[index] as u8;
+            }
+        }
+
+        count
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
     }
 }
 
